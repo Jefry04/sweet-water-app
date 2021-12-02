056e3eb157e66ce633baeba8e29194d1bfc57f8d
Jefferson <jefry04@gmail.com>
2021-12-02 17:25:55 -0500

    project structure and basic UI

---
+++ sw-ui/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from "next/link";
+
+
+function Home() {
+  return (
+      <>
+        <h2>FORMULARIOS MANTENIMIENTO</h2>
+        <Link href="/forms">Formulario 1</Link>
+        <h3>Formulario 2</h3>
+      </>
+  );
+}
+
+export default Home;
