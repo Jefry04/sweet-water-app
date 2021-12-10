939197b3d3f527245b9cf1af8bbc78cef8f81ff2
Jefferson <jefry04@gmail.com>
2021-12-10 14:43:48 -0500

    Login view with mobile first style

---
+++ sw-ui/src/components/LoginForm/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { authUser } from "../../lib/auth";
+import LoginFormView from "./LoginFormView";
+import { useRouter } from "next/router";
+
+const LoginForm = () => {
+  const [value, setValue] = useState({ username: "", password: "" });
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+  const [isAuthError, setIsAuthError] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [focused, setFocused] = useState(false);
+ 
+  const router = useRouter();
+  const { username, password } = value;
+
+  useEffect(() => {
+    if (isAuthenticate) {
+      router.push("/");
+    }
+  }, [isAuthenticate]);
+
+  useEffect(() => {
+    if (username.length > 0 && password.length > 0) {
+      setIsEmpty(false);
+      setIsAuthError(false);
+    } else {
+      setIsEmpty(true);
+    }
+  }, [value]);
+
+  const handleFocus =() =>{
+    setFocused(true);
+  };
+
+  const handleChange = (event) => {
+    setValue({
+      ...value,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { username, password } = value;
+    if (username === "admin" && password === "admin")
+      //    authUser(username, password); //enviar peticion al back
+      setIsAuthenticate(true)
+      else {setIsAuthError(true)};
+        };
+
+  return (
+    <LoginFormView
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      isEmpty={isEmpty}
+      isAuthError={isAuthError}
+      focused={focused}
+      handleFocus={handleFocus}
+    />
+  );
+};
+
+export default LoginForm;
