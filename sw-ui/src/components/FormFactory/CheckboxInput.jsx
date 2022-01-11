63cff2e448801b3c78aed0c506549d0167e6673c
Rogger Vasquez <rvasque3@gmail.com>
2022-01-11 09:14:51 -0500

    UI Refactor

--- sw-ui/src/components/FormFactory/CheckboxInput.jsx
+++ sw-ui/src/components/FormFactory/CheckboxInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const CheckboxInput = ({ questionLabel, optionsLabel }) => {
+export const CheckboxInput = ({ questionLabel }) => {
   return (
     <>
       <div className="checkbox-input">
