c35308d884141dac454c935b3bb1f0043d2e94b6
Jefferson Vasquez <jefry04@gmail.com>
2022-09-29 16:10:31 -0500

    change formview by role

--- sw-ui/src/pages/forms/FormList.jsx
+++ sw-ui/src/pages/forms/FormList.jsx
@@ -7,17 +7,25 @@ const FormsList = ({ user }) => {
   const formList = useFormData(`/roles?q=${user.roles}`);
 
   if (formList === null) return <div>failed to load</div>;
+
   return (
-    <div>
+    <>
       <h1>FORMULARIOS:</h1>
       <ul>
-        {formList?.forms?.map((items) => (
-          <li key={items._id}>
-            <Link href={`${"forms/"}${items._id}`}>{items.name}</Link>
-          </li>
+        {user?.roles?.map((role, idx) => (
+          <>
+            <h4 key={idx}>{role}</h4>
+            {formList?.forms
+              ?.filter((item) => item.allowedRoles.indexOf(role) >= 0)
+              .map((form) => (
+                <li key={form._id}>
+                  <Link href={`${"forms/"}${form._id}`}>{form.name}</Link>
+                </li>
+              ))}
+          </>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
