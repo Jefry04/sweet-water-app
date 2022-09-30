f1db19a4647f4478c8afd489ab71447cb20094bc
Jefferson Vasquez <jefry04@gmail.com>
2022-09-30 16:45:51 -0500

    create style structure

--- sw-ui/src/pages/admin/newform/CreateNewFormView.jsx
+++ sw-ui/src/pages/admin/newform/CreateNewFormView.jsx
@@ -1,5 +1,27 @@
-const CreateNewFormView = () => {
-  return <h1>Nuevo Formulario</h1>;
+import InputValidator from "components/InputValidator";
+import { usernameValidator } from "lib/validations";
+
+const CreateNewFormView = ({ formValue = {}, handleInputChange, validCb, signupError }) => {
+  const { name, firstName, lastName, password, roles } = formValue;
+
+  return (
+    <div className="container">
+      <div className="new-form-container">
+        <h1> Crear Nuevo Formulario</h1>
+        <form className="new-form"></form>
+        <InputValidator
+          name="name"
+          label="Nombre del formulario *"
+          type="text"
+          value={name}
+          onChange={handleInputChange}
+          validator={usernameValidator}
+          cb={validCb}
+          externalError={signupError}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CreateNewFormView;
